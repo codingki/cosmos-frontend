04d133a1b9b35697bfe958d8572458a58972ac73
Griko Nibras <git@griko.id>
2022-05-18 04:34:24 +0700

    chore(hooks): update useSeo typing

--- website/hooks/use-seo.tsx
+++ website/hooks/use-seo.tsx
@@ -1,7 +1,7 @@
 import type { NextSeoProps } from "next-seo";
 import { NextSeo } from "next-seo";
 
-export function useSeo<T>(props: T & NextSeoProps) {
+export function useSeo<T extends NextSeoProps>(props: T) {
   function Seo(extraProps: NextSeoProps) {
     return <NextSeo {...props} {...extraProps} />;
   }
