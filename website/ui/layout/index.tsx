965788a2f1f19e1c151c196403d8ac7806f1231e
Griko Nibras <git@griko.id>
2022-05-18 04:33:49 +0700

    chore(ui): remove layout skip nav

--- website/ui/layout/index.tsx
+++ website/ui/layout/index.tsx
@@ -1,5 +1,4 @@
 import { Box } from "@chakra-ui/react";
-import { SkipNavContent, SkipNavLink } from "@chakra-ui/skip-nav";
 import Head from "next/head";
 import type { ReactNode } from "react";
 import { MetaTags } from "ui/seo/meta-tags";
@@ -10,16 +9,12 @@ export interface LayoutProps {
 
 export function Layout({ children }: LayoutProps) {
   return (
-    <>
+    <Box as="main">
       <Head>
         <meta content="minimum-scale=1, initial-scale=1, width=device-width" name="viewport" />
       </Head>
       <MetaTags />
-      <SkipNavLink>Skip to content</SkipNavLink>
-      <Box as="main">
-        <SkipNavContent />
-        {children}
-      </Box>
-    </>
+      {children}
+    </Box>
   );
 }
