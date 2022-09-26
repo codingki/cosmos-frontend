0031b9fb1ca9d5c143a85753790b2595c11650ce
Nur Fikri <codingki@gmail.com>
2022-09-27 05:00:32 +0700

    graz example

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { GrazProvider, mainnetChains } from "graz";
 import type { AppProps } from "next/app";
 import { theme } from "styles/theme";
 import { Layout } from "ui/layout";
@@ -6,9 +7,15 @@ import { Layout } from "ui/layout";
 export default function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider resetCSS theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <GrazProvider
+        grazOptions={{
+          defaultChain: mainnetChains.cosmoshub,
+        }}
+      >
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </GrazProvider>
     </ChakraProvider>
   );
 }
--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -1,13 +1,36 @@
-import { Heading, Stack, Text } from "@chakra-ui/react";
+import { Box, Button, Center, HStack, Stack, Tag, TagLabel, TagLeftIcon, Text } from "@chakra-ui/react";
+import { useAccount, useActiveChain, useConnect, useDisconnect } from "graz";
 
 export default function HomePage() {
+  const { isConnected, data: account } = useAccount();
+  const { connect } = useConnect();
+  const { disconnect } = useDisconnect();
+  const activeChain = useActiveChain();
+
   return (
-    <Stack as="section" p={4}>
-      <Heading>Hello, world!</Heading>
-      <Text>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non minima tempora cum corporis laudantium ipsa sint
-        accusantium eos doloribus tenetur, aut sit, voluptas libero animi tempore ab rerum consectetur iste.
-      </Text>
-    </Stack>
+    <Center minH="100vh">
+      <Stack bgColor="whiteAlpha.100" boxShadow="md" maxW="md" p={4} rounded="md" spacing={4} w="full">
+        <HStack>
+          <Tag>
+            <TagLeftIcon as={Box} bgColor={isConnected ? "green.500" : "red.500"} boxSize={3} rounded="full" />
+            <TagLabel>{isConnected ? "Connected" : "Disconnected"}</TagLabel>
+          </Tag>
+        </HStack>
+        <Text>
+          Active chain id: <b>{activeChain?.chainId}</b>
+        </Text>
+        <Text>
+          Name: <b>{account?.name}</b>
+        </Text>
+        <Text noOfLines={1} wordBreak="break-all">
+          Address: <b>{account?.bech32Address}</b>
+        </Text>
+        <HStack align="end" pt={4}>
+          <Button onClick={() => (isConnected ? disconnect() : connect())}>
+            {isConnected ? "Disconnect" : "Connect Wallet"}
+          </Button>
+        </HStack>
+      </Stack>
+    </Center>
   );
 }
